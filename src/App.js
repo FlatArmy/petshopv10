5505f82d928aa7f19bbc074b3830f5e848f5af4e
alex2006 <2006.alex@gmail.com>
2024-08-25 23:22:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+
+/*imagenes*/
+import camaroteImg from './imagenes/camarote.jpg';
+import logoInicioImg from './imagenes/logoInicio.jpg';
+import habitacionImg from './imagenes/habitacion.jpg';
+import camarotedosImg from './imagenes/camarotedos.jpg';
+import camarotetripleImg from './imagenes/camarotetriple.jpg';
+import camaroteEstudioImg from './imagenes/camaroteEstudio.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <nav className="navbar">
+          <ul>
+            <li>Inicio</li>
+            <li>Acerca de nosotros(as)</li>
+            <li>Tienda</li>
+            <li>Contacto</li>
+            <li>Ayuda</li>
+            <li>Reseñas</li>
+            <li>Ubicaciones</li>
+            <li>Preguntas frecuentes</li>
+          </ul>
+        </nav>
       </header>
+      <main className="main-content">
+        <div className="image-section">
+          <img src={habitacionImg} alt="Imagen secundaria" className="secondary-image" />
+          <img src={logoInicioImg} alt="Producto 1" className="product-image" />
+        </div>
+
+        <div className="">
+          <img src={camaroteImg} alt="Producto 1" className="product-image" />
+          <img src={camarotedosImg} alt="Producto 2" className="product-image" />
+          <img src={camarotetripleImg} alt="Producto 3" className="product-image" />
+          <img src={camaroteEstudioImg} alt="Producto 3" className="product-image" />
+        </div>
+        <div>
+          <button className="shop-button">Ir a la tienda</button>
+        </div>
+      </main>
+      <footer className="footer">
+        <p>© 2024 Tu tienda de mascotas</p>
+      </footer>
     </div>
   );
 }
